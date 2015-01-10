c58a5aaffe367ee94782de8022e353d082883db0
Cafferoq <cafferoq@msoe.edu>
2015-01-10 15:40:50 -0600

    added success res.json's for messages.js methods

--- controllers/messages.js
+++ controllers/messages.js
@@ -4,22 +4,26 @@
 exports.recieveHash(req,res){
     var hash = req.body.hash;
     GAME.submitHash(hash);
+    res.json({status:"success"});
 };
 
 //takes RPS choice from opponent back-end
 exports.recieveRPSMessage(req,res){
     var message = req.body.message;
     GAME.submitDecision(message);
+    res.json({status:"success"});
 };
 
 //takes signature and passes it on
 exports.recieveSignature(req,res){
     var sig =req.body.signature;
     GAME.submitSignature(sig);
+    res.json({status:"success"});
 };
 
 //gets RPS choice from client
 exports.getMyDesicion(req,res){
     var decision = req.body.decision;
     GAME.submitMyDecision(decision);
+    res.json({status:"success"});
 };
