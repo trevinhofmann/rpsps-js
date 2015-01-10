4527b9ad88e7a55227973864e923f0c156ceac1d
Cafferoq <cafferoq@msoe.edu>
2015-01-10 15:51:32 -0600

    Fixed functions in messages.js
    stupid js...

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,28 +1,28 @@
 'use strict'; //not important, just include it
 
 //takes hash and passes it on
-exports.recieveHash(req,res){
+exports.recieveHash = function(req,res){
     var hash = req.body.hash;
     GAME.submitHash(hash);
     res.json({status:"success"});
 };
 
 //takes RPS choice from opponent back-end
-exports.recieveRPSMessage(req,res){
+exports.recieveRPSMessage= function(req,res){
     var message = req.body.message;
     GAME.submitDecision(message);
     res.json({status:"success"});
 };
 
 //takes signature and passes it on
-exports.recieveSignature(req,res){
+exports.recieveSignature= function(req,res){
     var sig =req.body.signature;
     GAME.submitSignature(sig);
     res.json({status:"success"});
 };
 
 //gets RPS choice from client
-exports.getMyDesicion(req,res){
+exports.getMyDesicion= function(req,res){
     var decision = req.body.decision;
     GAME.submitMyDecision(decision);
     res.json({status:"success"});
