128b209c9f465a53e99aaf6192392987f71eddbf
Jacob Robers <robersj@msoe.edu>
2015-01-10 15:20:34 -0600

    home, browse, and create

--- public/app/browse/browse.js
+++ public/app/browse/browse.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.browse', ['ngRoute'])
+angular.module('myApp.browse', ['ngRoute', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/browse', {
@@ -9,6 +9,29 @@ angular.module('myApp.browse', ['ngRoute'])
   });
 }])
 
-.controller('BrowseCtrl', [function() {
+.controller('BrowseCtrl', ['$scope', '$resource', function($scope, $resource) {
 
+
+	$scope.games = [
+		{
+			nickname: 'Bob',
+			number: '7',
+			bet: '100',
+			escrow: '10'
+		},
+		{
+			nickname: 'Trudy',
+			number: '9',
+			bet: '150',
+			escrow: '2'
+		},
+		{
+			nickname: 'Alice',
+			number: '7',
+			bet: '130',
+			escrow: '6'
+		},
+	];
+
+	console.log($scope.games);
 }]);
