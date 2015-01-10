a2fd66015e6f41a9f013c3fbc95e3987754abb13
Trevin Hofmann <trevinhofmann@gmail.com>
2015-01-10 13:07:12 -0600

    Begin fixing app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-module.exports = function(){
+var express = require('express');
+var app = express();
 
-  var express = require('express');
-  var app = express();
+// Use 'views' as the directory for HTML views.
+app.set('views', __dirname + '/views');
 
-  // Use 'views' as the directory for HTML views.
-  app.set('views', __dirname + '/views');
+// Render static files from the 'public' directory.
+app.use(express.static(__dirname + '/public/app/'));
 
-  // Render static files from the 'public' directory.
-  app.use(express.static(__dirname + '/public/app/'));
+// Use the routes.js script to point requests to the appropriate rendering script.
+require('./lib/routes')(app);
 
-  // Use the routes.js script to point requests to the appropriate rendering script.
-  require('./lib/routes')(app);
-
-  // Return the app
-  return app;
-
-}
+// List on port 7175;
+app.listen(7175);
