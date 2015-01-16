2d44e188ea4cdfd780254e0eed32501d8f4f7302
Trevin Hofmann <trevinhofmann@gmail.com>
2015-01-15 20:36:24 -0600

    Some formatting

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,4 +1,4 @@
-'use strict'; //not important, just include it
+'use strict';
 
 //takes hash and passes it on
 exports.receiveHash = function(req,res){
@@ -8,21 +8,21 @@ exports.receiveHash = function(req,res){
 };
 
 //takes RPS choice from opponent back-end
-exports.receiveRPSMessage= function(req,res){
+exports.receiveRPSMessage = function(req,res){
     var message = req.body.message;
     GAME.submitDecision(message);
     res.json({status:"success"});
 };
 
 //takes signature and passes it on
-exports.receiveSignature= function(req,res){
-    var sig =req.body.signature;
+exports.receiveSignature = function(req,res){
+    var sig = req.body.signature;
     GAME.submitSignature(sig);
     res.json({status:"success"});
 };
 
 //gets RPS choice from client
-exports.getMyDesicion= function(req,res){
+exports.getMyDesicion = function(req,res){
     var decision = req.body.decision;
     GAME.submitMyDecision(decision);
     res.json({status:"success"});
