efb889dc37f33053b8f509203dc7b67e68fd456a
Trevin Hofmann <trevinhofmann@gmail.com>
2015-01-15 20:42:26 -0600

    Some more formatting

--- controllers/matchMaking.js
+++ controllers/matchMaking.js
@@ -1,72 +1,63 @@
-'use strict'; //not important, just include it
+'use strict';
 
-//Takes in the ip you're connecting to and calls CONNECTIONS.connect to create the socket
+// Takes in the ip you're connecting to and calls CONNECTIONS.connect to create the socket
 exports.invite = function (req,res){
-    var ip = req.body.ip;
-    
-    CONNECTIONS.connect(ip, function(connStatus){
-        if (connStatus){
-            var success = "success";
-        }else{
-            var success = "failure";
-        };
-        
-        var connectionSuccess ={status:success};
-        //sends connection status back up to the client
-        res.json(connectionSuccess);
-    });
-        
+  var ip = req.body.ip;
+  CONNECTIONS.connect(ip, function(connStatus){
+    var status;
+    if (connStatus){
+      var status = "success";
+    } else{
+      var status = "failure";
+    }
+    var connectionSuccess = {status: status};
+    // Sends connection status back up to the client
+    res.json(connectionSuccess);
+  });
 };
 
-//Gets the list of games open for joining
+// Gets the list of games open for joining
 exports.listOpenGames = function(req,res){
-    CONNECTIONS.getOpenGames(function (openList){
-        res.json(openList);
-    });    
+  CONNECTIONS.getOpenGames(function (openList){
+    res.json(openList);
+  });    
 };
 
-//Creates a new game based on the input parameters (#games, and bet per game)
+// Creates a new game based on the input parameters (#games, and bet per game)
 exports.createGame = function(req,res){
-    var nickname = req.body.nickname;
-    var numGames = req.body.numGames;
-    var betPerGame = req.body.betPerGame;
-    
-    CONNECTIONS.createGame(nickname,numGames,betPerGame);
-    
-    var creationSuccess = {status:"success"};
-    //passes back that the creation was successful
-    res.json(creationSuccess);
+  var nickname = req.body.nickname;
+  var numGames = req.body.numGames;
+  var betPerGame = req.body.betPerGame;
+  CONNECTIONS.createGame(nickname,numGames,betPerGame);
+  var creationSuccess = {status:"success"};
+  //passes back that the creation was successful
+  res.json(creationSuccess);
 };
 
-//Requests to join a game and returns accept,denied,or pending
+// Requests to join a game and returns accept,denied,or pending
 exports.checkInvitation = function(req,res){
-    var invStatus = CONNECTIONS.checkInvitation();
-    var returnedStatus = {status:invStatus};
-    
-    res.json(returnedStatus);
+  var invStatus = CONNECTIONS.checkInvitation();
+  var returnedStatus = {status:invStatus};
+  res.json(returnedStatus);
 };
 
-//Gets a list of clients attempting to join your game.Returns an empty array if no clients attempting to join
+// Gets a list of clients attempting to join your game.Returns an empty array if no clients attempting to join
 exports.checkForInvitations = function(req,res){
-    res.json(CONNECTIONS.checkForInvitations());
+  res.json(CONNECTIONS.checkForInvitations());
 };
 
-//accepts an invitation
+// Accepts an invitation
 exports.acceptInvitation = function(req,res){
-    var nickname = req.body.nickname;
-    var ip = req.body.ip;
-    
-    CONNECTIONS.acceptInvitation(nickname,ip);
-    
-    res.json({status:"success"});
+  var nickname = req.body.nickname;
+  var ip = req.body.ip;
+  CONNECTIONS.acceptInvitation(nickname,ip);
+  res.json({status:"success"});
 };
 
-//declines an invitation
+// Declines an invitation
 exports.declineInvitation = function(req,res){
-    var nickname = req.body.nickname;
-    var ip = req.body.ip;
-    
-    CONNECTIONS.declineInvitation(nickname,ip);
-    
-    res.json({status:"success"});
+  var nickname = req.body.nickname;
+  var ip = req.body.ip;
+  CONNECTIONS.declineInvitation(nickname,ip);
+  res.json({status:"success"});
 };
--- controllers/messages.js
+++ controllers/messages.js
@@ -2,28 +2,28 @@
 
 //takes hash and passes it on
 exports.receiveHash = function(req,res){
-    var hash = req.body.hash;
-    GAME.submitHash(hash);
-    res.json({status:"success"});
+  var hash = req.body.hash;
+  GAME.submitHash(hash);
+  res.json({status:"success"});
 };
 
 //takes RPS choice from opponent back-end
 exports.receiveRPSMessage = function(req,res){
-    var message = req.body.message;
-    GAME.submitDecision(message);
-    res.json({status:"success"});
+  var message = req.body.message;
+  GAME.submitDecision(message);
+  res.json({status:"success"});
 };
 
 //takes signature and passes it on
 exports.receiveSignature = function(req,res){
-    var sig = req.body.signature;
-    GAME.submitSignature(sig);
-    res.json({status:"success"});
+  var sig = req.body.signature;
+  GAME.submitSignature(sig);
+  res.json({status:"success"});
 };
 
 //gets RPS choice from client
 exports.getMyDesicion = function(req,res){
-    var decision = req.body.decision;
-    GAME.submitMyDecision(decision);
-    res.json({status:"success"});
+  var decision = req.body.decision;
+  GAME.submitMyDecision(decision);
+  res.json({status:"success"});
 };
