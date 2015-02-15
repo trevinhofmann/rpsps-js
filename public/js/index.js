8b672ecc2c0daf734c07840fbba6539f94db59d8
Trevin Hofmann <trevinhofmann@gmail.com>
2015-02-15 15:09:09 -0600

    Add index.js

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(document).ready(function(){
+
+  $('#index-login').click(function(){
+    navigation.to('login');
+  });
+
+  $('#index-register').click(function(){
+    navigation.to('register');
+  });
+
+});
