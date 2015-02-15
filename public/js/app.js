063824b290179649e2adb5ffe48d361077dcbb3d
Trevin Hofmann <trevinhofmann@gmail.com>
2015-02-15 14:47:40 -0600

    Styling, and hide other views

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,10 @@
 var bitcore = require('bitcore');
 var message = require('bitcore-message');
 var mnemonic = require('bitcore-mnemonic');
+
+$(document).ready(function(){
+
+  $('#login').hide();
+  $('#register').hide();
+  
+});
