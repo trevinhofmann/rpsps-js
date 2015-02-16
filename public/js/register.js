ff376facdb64df1756584a8ab6ec0f0b39c9f84f
Trevin Hofmann <trevinhofmann@gmail.com>
2015-02-15 19:12:01 -0600

    Add register.js

---
+++ public/js/register.js
@@ -0,0 +1,29 @@
+'use strict';
+
+$(document).ready(function(){
+
+  var mnemonic = new Mnemonic();
+  $('#register-mnemonic').val(mnemonic.toString());
+  
+  $('#register-submit').prop('disabled', true);
+
+  $('#register-submit').click(function(){
+  
+    if ($('#register-mnemonic-confirm').val() == $('#register-mnemonic').val()){
+      var mnemonic = new Mnemonic($('#register-mnemonic-confirm').val());
+      xpriv = mnemonic.toHDPrivateKey();
+    }
+    
+  });
+  
+  $('#register-mnemonic-confirm').change(function(){
+  
+    if ($('#register-mnemonic-confirm').val() == $('#register-mnemonic').val()){
+      $('#register-submit').prop('disabled', false);
+    } else {
+      $('#register-submit').prop('disabled', true);
+    }
+    
+  });
+
+});
