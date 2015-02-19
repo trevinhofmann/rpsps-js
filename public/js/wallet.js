3d0d06354346948f2189103d6d6799048a2c6d3b
Trevin Hofmann <trevinhofmann@gmail.com>
2015-02-19 10:53:08 -0600

    Add getDepositAddress to Wallet

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -3,5 +3,12 @@
 function Wallet() {
 
   this.hdPrivateKey;
+  this.nextDepositIndex = 0;
 
 }
+
+Wallet.prototype.getDepositAddress = function(){
+  var privateKey = this.hdPrivateKey.derive('m/0/'+this.nextDepositIndex).privateKey;
+  var address = new Address(privateKey.publicKey);
+  return address;
+};
